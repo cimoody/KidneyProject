bbc1b8ca0eb9a09a93bce048b47c98f440b91086
cimoody <cristina.isadora.moody@gmail.com>
2015-08-13 10:14:23 -0400

    Script to process all the Kidney Data into R data file. Only use once.

---
+++ 01_getKidneyData.R
@@ -0,0 +1,16 @@
+## 01_getKidneyData.R
+
+## Kidney Data
+## Written by Cristina Moody
+## July 2015
+
+# Installing Libraries
+# source(file = "librariesVNL.R"); # If starting R for 1st time, else do below
+
+# Installing package to compare the two data tables
+require(compare);
+require(plyr);
+require(Hmisc);
+require(scales);
+# Working directory:
+wDir <- getwd();
